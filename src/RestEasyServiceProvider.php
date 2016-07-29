d1b7b873634d39e977571bf44b8f986011cadb7f
Christian Augustine <chriscalifornia88@gmail.com>
2016-07-28 21:09:13 -0500

    Added register method to service provider

--- src/RestEasyServiceProvider.php
+++ src/RestEasyServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Chriscalifornia88\RestEasy;
 
+use Chriscalifornia88\RestEasy\Commands\RestControllerMakeCommand;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -16,9 +17,9 @@ class RestEasyServiceProvider extends ServiceProvider
      * @var bool
      */
     protected $defer = true;
-    
+
     protected $commands = [
-        'Chriscalifornia88\RestEasy\Commands\RestControllerMakeCommand'
+        'command.rest-controller.make'
     ];
 
     /**
@@ -31,12 +32,28 @@ class RestEasyServiceProvider extends ServiceProvider
     }
 
     /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['command.rest-controller.make'];
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
+        $this->app['command.rest-controller.make'] = $this->app->share(
+            function () {
+                return new RestControllerMakeCommand();
+            }
+        );
+
         $this->commands($this->commands);
     }
 }
